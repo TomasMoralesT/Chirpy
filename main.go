d29633a8886991cd0546ea63ed0e08cffd06334e
GNight <tomas11_92@hotmail.com>
2025-03-17 17:35:44 -0500

    B: Created handlers.go, index.html, modified main.go

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	newMux := http.NewServeMux()
+	cfg := &apiConfig{}
+
+	newMux.HandleFunc("/healthz", healthHandler)
+
+	fileServer := http.FileServer(http.Dir("."))
+	newMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+
+	newMux.HandleFunc("/metrics", cfg.metricsHandler)
+
+	newMux.HandleFunc("/reset", cfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -21,4 +36,22 @@ func main() {
 		log.Fatal("Server error:", err)
 	}
 
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits counter reset"))
 }
