5c29913b333fef9109569ba5cca497bc262b3c22
GNight <tomas11_92@hotmail.com>
2025-03-18 17:29:45 -0500

    C

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,14 +16,16 @@ func main() {
 	newMux := http.NewServeMux()
 	cfg := &apiConfig{}
 
-	newMux.HandleFunc("/healthz", healthHandler)
+	newMux.HandleFunc("GET /api/healthz", healthHandler)
 
 	fileServer := http.FileServer(http.Dir("."))
 	newMux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
 
-	newMux.HandleFunc("/metrics", cfg.metricsHandler)
+	newMux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
 
-	newMux.HandleFunc("/reset", cfg.resetHandler)
+	newMux.HandleFunc("POST /admin/reset", cfg.resetHandler)
+
+	newMux.HandleFunc("/api/validate_chirp", cfg.validateChirp)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -47,11 +50,66 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	hits := cfg.fileserverHits.Load()
-	fmt.Fprintf(w, "Hits: %d", hits)
+
+	w.Header().Set("Content-Type", "text/html")
+	htmlResponse := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, hits)
+
+	fmt.Fprint(w, htmlResponse)
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Hits counter reset"))
+}
+
+func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		http.Error(w, `{"error": "Invalid request method"}`, http.StatusMethodNotAllowed)
+		return
+	}
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		response := map[string]string{"error": "Chirp is too long"}
+		respJSON, err := json.Marshal(response)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		w.Write(respJSON)
+		return
+	}
+	response := map[string]bool{"valid": true}
+	respJSON, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write(respJSON)
+	return
 }
