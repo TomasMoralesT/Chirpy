b771b16ae477dd9b6bc4f002740031f2f4a38747
GNight <tomas11_92@hotmail.com>
2025-03-19 13:11:31 -0500

    E

--- main.go
+++ main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+
+	"github.com/TomasMoralesT/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	queries        *database.Queries
+	platform       string
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not set in .env file")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Error opening database connection:", err)
+	}
+
+	dbQueries := database.New(db)
+
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		queries:        dbQueries,
+		platform:       os.Getenv("PLATFORM"),
+	}
+
 	newMux := http.NewServeMux()
-	cfg := &apiConfig{}
 
 	newMux.HandleFunc("GET /api/healthz", healthHandler)
 
@@ -26,6 +56,8 @@ func main() {
 
 	newMux.HandleFunc("POST /admin/reset", cfg.resetHandler)
 
+	newMux.HandleFunc("POST /api/users", cfg.createUser)
+
 	newMux.HandleFunc("/api/validate_chirp", cfg.validateChirp)
 
 	server := &http.Server{
@@ -35,7 +67,7 @@ func main() {
 
 	fmt.Println("Server starting on :8080...")
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal("Server error:", err)
 	}
@@ -64,9 +96,54 @@ func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	cfg.fileserverHits.Store(0)
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "This endpoint is only available in development mode")
+		return
+	}
+
+	err := cfg.queries.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to reset database")
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits counter reset"))
+
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Error decoding request body")
+		return
+	}
+
+	user, err := cfg.queries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Error creating user")
+		return
+	}
+
+	responseUser := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201 Created
+
+	json.NewEncoder(w).Encode(responseUser)
 }
 
 func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
